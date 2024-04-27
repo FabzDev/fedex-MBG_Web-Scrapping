41873a9a0257c3591d8ef80e2eda68fda90dfb8a
Fabio <fabioescobarardila@hotmail.com>
2024-04-26 23:03:52 -0500

    V1. GSRscraping . 100% functional

--- fedexGSRBuffer.ts
+++ fedexGSRBuffer.ts
@@ -4,17 +4,19 @@ import { Browser, Page } from "puppeteer";
 import bufferList from "./bufferGsrList.json";
 import { GsrInterface } from "./gsr.interface";
 import { createWorker } from "tesseract.js";
+var XLSX = require("xlsx");
+import * as fs from 'fs';
+
 
 // PREPARANDO PUPPETEER Y VARIABLES.
 puppeteer.use(StealthPlugin());
 const url =
   "https://www.fedex.com/servlet/InvoiceServlet?link=4&jsp_name=adjustment&orig_country=US&language=english/";
 const gsrArray: string[] = [];
-const clip = { x: 290, y: 190, width: 550, height: 120 };
-
+const clip = { x: 290, y: 190, width: 700, height: 120 };
 
 // MAIN ALGORITM
-async function gsr(gsr: GsrInterface, array: string[]){
+async function gsr(gsr: GsrInterface, array: string[]) {
   const trackingNumber: string = gsr["TRACKING NUMBER"];
   const invoiceNumber: string = gsr["INVOICE NUMBER"];
 
@@ -26,81 +28,123 @@ async function gsr(gsr: GsrInterface, array: string[]){
   // START PUPPET PAGE 1
   await page.goto(url);
 
-  await delay(500);
+  // await delay(500);
   await page.click('input[value="E"]');
 
-  await delay(500);
+  // await delay(500);
   await page.click('input[value="invoice"]');
 
-  await delay(500);
+  // await delay(500);
   await page.click('input[name="NewReq"]');
   await delay(2000);
   // END PUPPET PAGE 1
-  
+
   try {
     // START PUPPET PAGE 2
     await page.click('input[name="tracking_nbr"]', { clickCount: 3 });
     await page.keyboard.press("Backspace");
     await page.type('input[name="tracking_nbr"]', trackingNumber);
-    await delay(500);
+    // await delay(500);
 
     await page.click('input[name="invoice_nbr"]', { clickCount: 3 });
     await page.keyboard.press("Backspace");
     await page.type('input[name="invoice_nbr"]', invoiceNumber);
-    await delay(500);
+    // await delay(500);
 
     await page.click('input[value="Send Request"]');
-    await page.waitForNavigation({ timeout: 33000 });
+    await page.waitForNavigation({ timeout: 15000 });
     // END PUPPET PAGE 2
 
     // START PUPPET PAGE 3 (SCREENSHOT)
-    await delay(2000);
+    // await delay(2000);
     const buffImg = await page.screenshot({
       encoding: "binary",
       clip: clip,
-      path: `./GSRimgs/${trackingNumber}_${invoiceNumber}.png`,
+      // path: `./GSRimgs/${trackingNumber}_${invoiceNumber}.png`,
     });
 
     const worker = await createWorker("eng");
     const ret = await worker.recognize(buffImg);
     array.push("\n<-----Inicio\n" + ret.data.text + "\nFin----->");
-    console.log("\n<-----Inicio\n" + ret.data.text + "\nFin----->");
+    // console.log("\n<-----Inicio\n" + ret.data.text + "\nFin----->");
     await worker.terminate();
-    await delay(1000);
+    // await delay(1000);
     // END PUPPET PAGE 3 (SCREENSHOT)
-
   } catch {
     console.log(
       "Error catched on " + gsr["TRACKING NUMBER"] + "_" + gsr["INVOICE NUMBER"]
     );
   }
-  
+
   await browser.close();
+}
 
-  
-};
+function mapData(strArr: string[]) {
+  const finalArray = strArr.map((message) => {
+    const motivoRegex = /reason: (.*?)(?=\n)/;
+    const motivoMatch = message.match(motivoRegex);
+    const motivo = motivoMatch ? motivoMatch[1] : "";
 
-function printGSRArr(strArr: string[]) {
-  for (const str of strArr) {
-    console.log(str);
-  }
+    const trackNumberRegex = /Tracking Number:\s+(.*?)(?=\s)/;
+    const trackNumberMatch = message.match(trackNumberRegex);
+    const trackNumber = trackNumberMatch ? trackNumberMatch[1] : "";
+
+    if (message.includes("ADJUSTED")) return `${trackNumber} - APPROVED`;
+
+    // console.log(`${trackNumber} - ${motivo}`);
+    
+    return `${trackNumber} - ${motivo}`;
+  });
+  const jsonData = JSON.stringify(finalArray, null, 2);
+  fs.writeFileSync('datos.json', jsonData);
+  createExcelFile(finalArray);
 }
 
 async function delay(ms: number) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
-async function main(buffList: GsrInterface[], gsrArr: string[] ) {
-  
-  for(const gsrInfo of buffList){
-    await gsr(gsrInfo, gsrArr);    
+function createExcelFile(dataArray: string[]) {
+  // Crear un nuevo libro de Excel
+  const workbook = XLSX.utils.book_new();
+
+  // Crear una nueva hoja
+  const worksheet = XLSX.utils.aoa_to_sheet([
+    ["Tracking Number", "Description"],
+  ]);
+
+  // Iterar sobre cada elemento del arreglo
+  dataArray.forEach((dataString) => {
+    // Separar el string por el caracter "-"
+    const partes = dataString.split(" - ");
+
+    // Agregar una fila con los datos a la hoja
+    const lastRow = XLSX.utils.sheet_add_aoa(worksheet, [partes], {
+      origin: -1,
+    });
+  });
+
+  // Agregar la hoja al libro
+  XLSX.utils.book_append_sheet(workbook, worksheet, "Sheet1");
+
+  try {
+    // Guardar el libro como archivo Excel
+    XLSX.writeFile(workbook, "test.xlsx");
+  } catch (error) {
+    console.log(error);
+    XLSX.writeFile(workbook, "test2.xlsx");
+  }
+}
+
+async function main(buffList: GsrInterface[], gsrArr: string[]) {
+  for (const gsrInfo of buffList) {
+    await gsr(gsrInfo, gsrArr);
   }
 
-  printGSRArr(gsrArr)
+  mapData(gsrArr);
 }
 
 main(bufferList, gsrArray);
-
 
 // console.log("continue! " + trackNumber + " - " + invoiceNumber);
 
